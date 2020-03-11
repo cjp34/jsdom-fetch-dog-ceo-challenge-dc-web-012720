d268e0a7c59a1418985d91ff19e05da464b41a3e
cjp34 <carlparm@gmail.com>
2020-03-11 17:57:18 -0400

    All but filter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,72 @@
-console.log('%c HI', 'color: firebrick')
+// "https://dog.ceo/api/breeds/image/random/4"
+// 'https://dog.ceo/api/breeds/list/all'
+
+document.addEventListener("DOMContentLoaded", () => {
+    getPictures()
+    getBreeds()
+    let selector = document.getElementById('breed-dropdown')
+        selector.addEventListener("change", filterBreeds)
+})
+
+
+
+function getPictures() {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+        .then(resp => resp.json())
+        .then(json => imageGetter(json))
+}
+
+function imageGetter(json) {
+    let imageArea = document.getElementById('dog-image-container')
+    json.message.forEach(imageURL => {
+        let imageNew = document.createElement('img')
+        imageNew.src = imageURL
+        imageNew.style.height = '200px'
+        imageArea.appendChild(imageNew)
+    })
+}
+
+
+function getBreeds() {
+    fetch('https://dog.ceo/api/breeds/list/all')
+        .then(resp => resp.json())
+        .then(json => breedGetter(json))
+}
+
+
+function breedGetter(json) {
+    let breedArea = document.getElementById('dog-breeds')
+    let breeds = (Object.keys(json.message))
+    let allBreeds = breeds.forEach(breed => {
+        let breedNew = document.createElement('li')
+        breedNew.innerText = breed
+        breedNew.addEventListener("click", () => {
+            event.target.style.color = 'red'
+        })
+        breedArea.appendChild(breedNew)
+    })
+}
+
+function filterBreeds(event) {
+    let allTheBreeds = function getBreedsForFilter() {
+        return fetch('https://dog.ceo/api/breeds/list/all')
+            .then(resp => resp.json())
+    }
+    debugger
+    let breedsArray = Array.from(allTheBreeds)
+    let breedsList = document.getElementById('dog-breeds')
+        breedsList.innerHTML = ''
+    // debugger   
+    let letter = event.target.value
+    breedsArray.forEach( breed => {
+        if (breed.innerText.charAt(0) === letter) {
+            // let breedNew = document.createElement('li')
+            // breedNew.innerText = breed
+            breedsList.appendChild(breed)
+        }
+    })
+
+    // if letter === 'a'
+
+}
+
